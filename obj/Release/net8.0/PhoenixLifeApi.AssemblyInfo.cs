5b33c7cbe181885131548441e655e9706491c00b
Joshua Sullivan <jsulli0527@gmail.com>
2025-03-10 20:33:30 -0400

    Syncing Main Branch

--- obj/Release/net8.0/PhoenixLifeApi.AssemblyInfo.cs
+++ obj/Release/net8.0/PhoenixLifeApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoenixLifeApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+807a300e2898b041e7365cb11f460d03ded4aebf")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoenixLifeApi")]
